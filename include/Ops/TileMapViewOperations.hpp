75ddc46fb2c1eaec3955c1d85a43679796b1d446
pmarathe25 <pmarathe25@gmail.com>
2018-01-01 04:50:41 -0500

    Employs perfect forwarding

--- include/Ops/TileMapViewOperations.hpp
+++ include/Ops/TileMapViewOperations.hpp
@@ -1,19 +1,16 @@
 #ifndef TILE_MAP_VIEW_OPERATIONS_H
 #define TILE_MAP_VIEW_OPERATIONS_H
 #include "../OpStructs/TileMapView.hpp"
+#include <type_traits>
 #include <cassert>
 
 namespace StealthTileMap {
     template <typename LHS>
-    constexpr auto layer(const LHS& lhs, int layerNum = 0) {
-        assert(layerNum < internal::traits<LHS>::height);
-        return TileMapView<internal::traits<LHS>::width, internal::traits<LHS>::length, 1, LHS>{lhs, 0, 0, layerNum};
-    }
-
-    template <typename LHS>
-    constexpr auto layer(LHS& lhs, int layerNum = 0) {
-        assert(layerNum < internal::traits<LHS>::height);
-        return TileMapView<internal::traits<LHS>::width, internal::traits<LHS>::length, 1, LHS>{lhs, 0, 0, layerNum};
+    constexpr auto layer(LHS&& lhs, int layerNum = 0) {
+        assert(layerNum < internal::traits<typename std::remove_reference<LHS>::type>::height);
+        return TileMapView<internal::traits<typename std::remove_reference<LHS>::type>::width,
+            internal::traits<typename std::remove_reference<LHS>::type>::length, 1,
+            typename std::remove_reference<LHS>::type>{std::forward<LHS>(lhs), 0, 0, layerNum};
     }
 
 } /* StealthTileMap */
