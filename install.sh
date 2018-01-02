e98222c7c37fb1cadcb639de358c054ac098f479
pmarathe25 <pmarathe25@gmail.com>
2018-01-01 20:16:01 -0500

    Adds better installation script

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-sudo mkdir -p /usr/local/include/TileMap/
-sudo rm -r /usr/local/include/TileMap/*
-sudo cp -r ./include/* /usr/local/include/TileMap/
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+sudo ln -s $DIR/include/ /usr/local/include/TileMap
