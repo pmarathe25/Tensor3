6810b427e8a56b11e3bd38f754b87140385cf864
pmarathe25 <pmarathe25@gmail.com>
2018-03-20 22:10:27 -0700

    Adds better testing framework, might want to split into completely separate project

--- test/test.cpp
+++ test/test.cpp
@@ -3,44 +3,115 @@
 #include <iostream>
 #include <algorithm>
 
-constexpr static int kTEST_WIDTH = 25;
-constexpr static int kTEST_HEIGHT = 25;
-constexpr static int kTEST_SIZE = kTEST_WIDTH * kTEST_HEIGHT;
-
+constexpr int kTEST_WIDTH = 25;
+constexpr int kTEST_LENGTH = 25;
+constexpr int kTEST_HEIGHT = 25;
+constexpr int kTEST_AREA = kTEST_WIDTH * kTEST_LENGTH;
+constexpr int kTEST_SIZE = kTEST_AREA * kTEST_HEIGHT;
 
 template <int width = 1, int length = 1, int height = 1>
-constexpr auto SequentialTileMapF() noexcept {
+constexpr auto SequentialTileMapF(int startValue = 0) noexcept {
     Stealth::TileMapF<width, length, height> out{};
-    std::iota(out.begin(), out.end(), 0);
+    std::iota(out.begin(), out.end(), startValue);
     return out;
 }
+
+template <typename Callable>
+constexpr bool runTest(const Callable& test) {
+    // Returns true if the test passed, false otherwise.
+    auto result = test();
+    if (result.errorCode) {
+        std::cout << "Test " << result.testName << " failed with error code " << result.errorCode << '\n';
+        return false;
+    }
+    return true;
+}
+
+struct TestResult {
+    TestResult(int errorCode, std::string testName = __builtin_FUNCTION()) : testName{testName}, errorCode{errorCode} { }
+    int errorCode;
+    std::string testName;
+};
 
 // int testBinaryOps() {
 //
 // }
 
 namespace Block {
-    int testReshape() {
+    // Choose blocks starting about halfway in the TileMap.
+    constexpr int kBLOCK_X = kTEST_WIDTH / 2, kBLOCK_Y = kTEST_LENGTH / 2, kBLOCK_Z = kTEST_HEIGHT / 2;
+    constexpr int kBLOCK_WIDTH = kTEST_WIDTH / 4, kBLOCK_LENGTH = kTEST_LENGTH / 4, kBLOCK_HEIGHT = kTEST_HEIGHT / 4;
+    // In a sequential TileMap, what should the value be at this location?
+    constexpr int kSTART_VALUE1D = kBLOCK_X;
+    constexpr int kSTART_VALUE2D = kBLOCK_X + kBLOCK_Y * kTEST_WIDTH;
+    constexpr int kSTART_VALUE3D = kBLOCK_X + kBLOCK_Y * kTEST_WIDTH + kBLOCK_Z * kTEST_AREA;
+
+    TestResult testReshape() {
         // First generate a TileMap with sequential values.
-        auto reshapeTest0 = SequentialTileMapF<kTEST_WIDTH, kTEST_HEIGHT>();
+        auto reshapeTest0 = SequentialTileMapF<kTEST_WIDTH, kTEST_LENGTH>();
         // Try to reshape.
-        auto reshapeTest1 = SequentialTileMapF<kTEST_SIZE>();
+        auto reshapeTest1 = SequentialTileMapF<kTEST_AREA>();
         // Make sure that it worked.
-        auto checkReshape0 = (Stealth::reshape<kTEST_SIZE>(reshapeTest0) == reshapeTest1).eval();
-        if (std::all_of(checkReshape0.cbegin(), checkReshape0.cend(), [](bool in) { return in; })) {
-            // Test passed, exit with no error code.
-            return 0;
-        } else {
-            return 1;
-        }
+        auto checkReshape0 = (Stealth::reshape<kTEST_AREA>(reshapeTest0) == reshapeTest1).eval();
+        int error = not std::all_of(checkReshape0.cbegin(), checkReshape0.cend(), [](bool in) { return in; });
+        return TestResult{error};
     }
 
-    int testBlockOps() {
-        int numFailed = 0;
-        numFailed += testReshape();
-        return numFailed;
+    TestResult test1DBlockFrom1D() {
+
+    }
+
+    TestResult test1DBlockFrom2D() {
+
+    }
+
+    TestResult test1DBlockFrom3D() {
+
+    }
+
+    TestResult test2DBlockFrom2D() {
+        auto blockTest0 = SequentialTileMapF<kTEST_WIDTH, kTEST_LENGTH>();
+        // Retrieve a block.
+        auto blockTest1 = Stealth::block<kBLOCK_WIDTH, kBLOCK_LENGTH>(blockTest0, kBLOCK_X, kBLOCK_Y);
+        // Check whether every digit is consecutive row-by-row and that all the rows start on the correct value.
+        int numIncorrect = 0;
+        for (int y = 0; y < blockTest1.length(); ++y) {
+            numIncorrect += blockTest1(0, y) != (kSTART_VALUE2D + kTEST_WIDTH * y);
+            for (int x = 1; x < blockTest1.width(); ++x) {
+                numIncorrect += blockTest1(x, y) != blockTest1(x - 1, y) + 1;
+            }
+        }
+        return TestResult{numIncorrect};
+    }
+
+    TestResult test2DBlockFrom3D() {
+        auto blockTest0 = SequentialTileMapF<kTEST_WIDTH, kTEST_LENGTH, kTEST_HEIGHT>();
+        // Retrieve a block.
+        auto blockTest1 = Stealth::block<kBLOCK_WIDTH, kBLOCK_LENGTH>(blockTest0, kBLOCK_X, kBLOCK_Y, kBLOCK_Z);
+        // Check whether every digit is consecutive row-by-row and that all the rows start on the correct value.
+        int numIncorrect = 0;
+        for (int y = 0; y < blockTest1.length(); ++y) {
+            numIncorrect += blockTest1(0, y) != (kSTART_VALUE3D + kTEST_WIDTH * y);
+            for (int x = 1; x < blockTest1.width(); ++x) {
+                numIncorrect += blockTest1(x, y) != blockTest1(x - 1, y) + 1;
+            }
+        }
+        return TestResult{numIncorrect};
+    }
+
+    int test3DBlockFrom3D() {
+
     }
 } /* Block */
+
+bool testBlockOps() {
+    bool allTestsPassed = true;
+    allTestsPassed &= runTest(Block::testReshape);
+
+    allTestsPassed &= runTest(Block::test2DBlockFrom2D);
+    allTestsPassed &= runTest(Block::test2DBlockFrom3D);
+    return allTestsPassed;
+}
 
 //
 // int testUnaryOps() {
@@ -100,15 +171,13 @@ namespace Block {
 
 
 int main() {
-    int numFailed = 0;
+    bool allTestsPassed = true;
     // Block op tests
-    numFailed += Block::testBlockOps();
+    allTestsPassed &= testBlockOps();
     // numFailed += testUnaryOps();
     // numFailed += testTemporaryExpressionPersistence();
     // numFailed += testExpressionIndexing();
-    if (numFailed) {
-        std::cout << "One or more tests failed!" << '\n';
-    } else {
+    if (allTestsPassed) {
         std::cout << "All tests passed!" << '\n';
     }
     //
