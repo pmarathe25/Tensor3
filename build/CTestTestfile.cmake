3024631df9cf4826271a87a4673ee62103d966a2
pmarathe25 <pmarathe25@gmail.com>
2018-06-07 19:28:57 -0700

    Adds interface file

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/pranav/C++/Stealth/Tensor3
+# Build directory: /home/pranav/C++/Stealth/Tensor3/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test "/home/pranav/C++/Stealth/Tensor3/build/test0")
